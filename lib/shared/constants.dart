76ff96c55f712404430ebaf6636e6c05126fea50
unknown <mahekmj123@gmail.com>
2023-04-04 07:57:54 +0530

    firebase added

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:967527131507:web:bb7580efba961d72c07d06";
+  static String apiKey = "AIzaSyC1wmPbAjuMpX2DBFEEuN7uhiBnAEKb73w";
+  static String messagingSenderId = "967527131507";
+  static String projectId = "chat-app-fcb2c";
+}
